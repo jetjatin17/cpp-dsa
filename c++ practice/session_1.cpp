e75ff76704faff82317d17b90a3ee4b20d99975e
jetjatin17 <jsahrawat200817@gmail.com>
2026-09-01 12:37:22 +0530

    this is a for loop use code.

--- c++ practice/session_1.cpp
+++ c++ practice/session_1.cpp
@@ -104,11 +104,27 @@ using namespace std;
 
 // string as an input.
 
+// int main(){
+//     string name;
+//     cout << "enter your name : ";
+//     cin >> name;
+//     cout << name << " is a very great man." << endl;
+//     return 0;
+// }
+
+
+
+// for loop in c++.
+
 int main(){
-    string name;
-    cout << "enter your name : ";
-    cin >> name;
-    cout << name << " is a very great man." << endl;
+    int n,i,sum = 0;
+    cout << "enter the number : ";
+    cin >> n;
+    for (i = 1; i <= n; i++){
+        if (!(i%2 == 0)){
+            cout << i << endl;
+            sum = sum + i;            
+        }
+    }cout << sum;
     return 0;
 }
-
