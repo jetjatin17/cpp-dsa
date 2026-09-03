8e0d0d2ba99a73e3b7950377278c3b143483ef7b
jetjatin17 <jsahrawat200817@gmail.com>
2026-09-04 00:27:14 +0530

    pattern problems.

---
+++ c++ practice/pattern.cpp
@@ -0,0 +1,95 @@
+// for n = 4
+// 1234
+// 1234
+// 1234
+// 1234
+
+#include<iostream>
+using namespace std;
+
+// int main(){
+//     int n;
+//     cout << "enter the number : ";
+//     cin >> n;
+
+//     for (int i = 1; i<=n; i++){
+//         for (int j = 1;j<=n; j++){
+//             cout << j;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+// int main(){
+//     int n;
+//     cout << "enter the number : ";
+//     cin >> n;
+    
+//     for (int i = 1; i<=n; i++){
+//         for (char j = 65; j<(n+65); j++){
+//             cout << j;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+
+// for n = 3.
+// 123
+// 456
+// 789           the numbers continue.
+
+// int main(){
+//     int n;
+//     cout << "enter the number : ";
+//     cin >> n;
+
+//     for (int i = 1; i<=n; i++){
+//         int k = 1+((i-1)*n);
+//         for (int j = k; j < (k+n); j++){
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+// int main(){
+//     int n;
+//     cout << "enter the number : ";
+//     cin >> n;
+//     int num = 1;
+//     for (int i=0; i<n; i++){
+//         for (int j=0; j<n; j++){
+//             cout << num << " ";
+//             num++;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+// triangle pattern for n = 3.
+// *
+// **
+// ***
+
+int main(){
+    int n;
+    cout << "enter the number : ";
+    cin >> n;
+    for (int i = 1; i<=n; i++){
+        for (int j = 0; j<i; j++){
+            cout << "* ";
+        }cout << endl;
+    }    
+    return 0;
+}
