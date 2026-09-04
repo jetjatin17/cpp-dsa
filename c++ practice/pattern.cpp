480afbf5a31addefa81d84d4ee9a896250d08555
jetjatin17 <jsahrawat200817@gmail.com>
2026-09-05 01:43:16 +0530

    some more patterns.

--- c++ practice/pattern.cpp
+++ c++ practice/pattern.cpp
@@ -207,19 +207,86 @@ using namespace std;
 
 
 
+// int main(){
+//     int n;
+//     cout << "enter the number : ";
+//     cin >> n;
+//     char ch = 'A';
+//     for (int i = 0; i<n; i++){
+//         for (int k = 0; k<i; k++){
+//             cout << " "; 
+//         }
+//         for (int j = 0; j<(n-i) ; j++){
+//             cout << ch;
+//         }cout << endl;
+//         ch+=1;
+//     }
+//     return 0;
+// }
+
+
+
+// pyramid  pattern for n = 3.
+
+//     1
+//    121
+//   12321  
+
+// int main(){
+//     int n;
+//     cout << "enter the number : ";
+//     cin >> n;
+//     for (int i = 1; i<=n; i++){
+//         for (int j = 1; j<=(n-i); j++){
+//             cout << " ";
+//         }
+//         for (int j=0; j<i; j++){
+//             cout << (j+1);
+//         }
+//         for (int j = i-1; j>0; j--){
+//             cout << j;
+//         }cout << endl;
+//     }
+//     return 0;
+// }
+
+
+
+
+// hollow diamond pattern for  n = 4.
+
+//    *
+//   * *
+//  *   *
+// *     *
+//  *   *
+//   * *
+//    *
+
 int main(){
     int n;
     cout << "enter the number : ";
     cin >> n;
-    char ch = 'A';
-    for (int i = 0; i<n; i++){
-        for (int k = 0; k<i; k++){
-            cout << " "; 
-        }
-        for (int j = 0; j<(n-i) ; j++){
-            cout << ch;
+    for (int i=1; i<=n; i++){
+        for (int j=0; j<(n-i); j++){
+            cout << " ";
+        }cout << "*";
+        if (i!=1){
+            for (int j=1; j<=(((i-1)*2)-1); j++){
+                cout << " ";
+            }cout << "*";
         }cout << endl;
-        ch+=1;
+    }
+    for (int i=1;i<n;i++){
+        for (int j=0; j<i; j++){
+            cout << " ";
+        }cout << "*";
+        if (i!=(n-1)){
+            for (int j=(((n-(i+1))*2)-1); j>0; j--){
+                cout << " ";
+            }cout << "*";
+        }cout << endl;
     }
     return 0;
 }
+
